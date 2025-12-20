ccbabc950f8d3964574a84d921e87296a9f031e3
Meharop <141541946+Meharop@users.noreply.github.com>
2025-12-20 09:52:49 +0000

    datatypes

---
+++ .devcontainer/01-BASICS/datatypes.js
@@ -0,0 +1,8 @@
+"use strict";
+let name = "shahzaib"
+let age = 18
+let  isLogin = "true"
+
+console.log(typeof undefined);//type is undefined
+console.log(typeof null);//type is object
+
---
+++ .devcontainer/01-BASICS/varriable.js
@@ -0,0 +1,14 @@
+const accountId = 143443
+let accountEmail = "shahz@gmail.com"
+var accountPassword = "34423"
+accountCity ="kasur"
+let accountState;
+
+accountEmail = "dh@fg.com"
+accountPassword = "86786877"
+accountCity = "lahore"
+
+// accountId  = 74737
+console.log(accountId);
+/* preffer not to use var bcz of scoop problem*/
+console.table([accountId,accountEmail,accountPassword,accountCity , accountState])
