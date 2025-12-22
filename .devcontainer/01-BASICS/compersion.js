18467f04210b6f54173901a909423238b502aa98
Meharop <141541946+Meharop@users.noreply.github.com>
2025-12-22 08:59:24 +0000

    comperision

---
+++ .devcontainer/01-BASICS/compersion.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// we do not have to do that kind of compersision
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// === strict check
+console.log("2" === 0);
