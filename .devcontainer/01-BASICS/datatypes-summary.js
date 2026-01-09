8c8ba8611531ae373809e6c755106e5a8fc12ecf
Meharop <141541946+Meharop@users.noreply.github.com>
2026-01-09 18:31:04 +0000

    DATATYPE-SUMMARY

---
+++ .devcontainer/01-BASICS/datatypes-summary.js
@@ -0,0 +1,37 @@
+// premitive
+
+// 7 types : string , Number , Boolean , null, undefined , symbol , bigint 
+const score = 100 
+const scorevvalue = 1000.0
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail ;
+const id = symbol('123')
+const anotherid = symbol('123')
+console.log(id===anotherid );
+
+const bigNumber = 647476n
+
+
+
+
+
+// Refencetype ( non-premitive) 
+
+// Array , Object , Function 
+//   JavaScript is a dynamically typed language because the type of a variable is determined at runtime (when the code is executing) rather than at compile time (before the code runs).
+
+
+const heros = ["jinnah","Iqbal","gandhi"]
+let myobject = {
+
+    name: "shahzaib",
+    age: 20 ,
+}
+
+
+ const myFunction = function () {
+
+    console.log("hello world");
+    // data types of npn primitive is OBJECT
+ }
