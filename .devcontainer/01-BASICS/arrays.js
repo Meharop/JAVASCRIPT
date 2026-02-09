6c798f03b9b1de9064cb81bbd80ff072250e688d
Meharop <141541946+Meharop@users.noreply.github.com>
2026-02-09 11:04:26 +0000

    abhi arrays ki basics ki hn

---
+++ .devcontainer/01-BASICS/arrays.js
@@ -0,0 +1,44 @@
+// array 
+const newArr = ['a', 'b', 'c', 1, 2, 3, "shybi"] // ARRAYS CAN BE CHANGED LATER
+const myHeros = ['Flash', "BatMan", "spiderMan"]
+const array2 = new Array(1, 2, 3, 4)
+//console.log(newArr[1]); // WE SHOULD CALL ARRAY FROM 0 INDEX MEAN VALUE SHOULD BE START FROM ZERO
+const secondArray = new Array(5, 6, 6, 7)
+//console.log(secondArray[3])
+//console.log(myHeros[1],newArr[4]
+
+
+//  myHeros.push("sayang" , "shahzaib")
+// myHeros.pop()
+
+// console.log(myHeros);
+
+const myArr = [0,1,2,3,4,5,6]
+
+// myArr.unshift(9)
+// myHeros.unshift("shahzaib")
+// myArr.shift()
+// myHeros.shift()
+
+ console.log(myHeros.indexOf("BatMan"));
+// console.log(myArr);
+
+// const newArr2 = myArr.join()
+// console.log(myArr);
+// console.log(newArr2);
+// console.log(typeof newArr2);
+
+// slice , splice
+console.log("A" , myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+
+
+console.log("B" , myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C" , myArr);
+console.log(myn2);
+// console.log(myArr);
