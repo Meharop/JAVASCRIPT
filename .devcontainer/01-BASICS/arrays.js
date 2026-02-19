9c06c9270064ff3577dfadcb994d33ba1d7a17c0
Meharop <141541946+Meharop@users.noreply.github.com>
2026-02-19 08:31:31 +0000

    object basics done

--- .devcontainer/01-BASICS/arrays.js
+++ .devcontainer/01-BASICS/arrays.js
@@ -29,16 +29,25 @@ const myArr = [0,1,2,3,4,5,6]
 // console.log(typeof newArr2);
 
 // slice , splice
-console.log("A" , myArr);
+// console.log("A" , myArr);
 
-const myn1 = myArr.slice(1,3)
+// const myn1 = myArr.slice(1,3)
 
-console.log(myn1);
+// console.log(myn1);
 
 
-console.log("B" , myArr);
+// console.log("B" , myArr);
 
-const myn2 = myArr.splice(1,3)
-console.log("C" , myArr);
-console.log(myn2);
+// const myn2 = myArr.splice(1,3)
+// console.log("C" , myArr);
+// console.log(myn2);
 // console.log(myArr);
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc = ["superman","batman","flash"]
+
+// marvel_heros.push(dc)
+// console.log(marvel_heros);
+// const allHeros = marvel_heros.concat(dc)
+// console.log(allHemaros);
+const all_new_heros = [...marvel_heros,...dc]
+log
