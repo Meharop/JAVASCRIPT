289ba84e2b67d21c8d01a59822498876344514cd
Meharop <141541946+Meharop@users.noreply.github.com>
2026-03-08 20:37:45 +0000

    functions yahiiiii

---
+++ .devcontainer/01-BASICS/functions.js
@@ -0,0 +1,51 @@
+
+
+
+function callMyName(){
+
+console.log("S")
+console.log("h")
+console.log("a")
+console.log("h")
+console.log("z")
+console.log("a")
+console.log("i")
+console.log("b")
+
+}
+
+
+// callMyName()
+// function addTwoNumbers(number1,number2){
+
+//     console.log(number1 + number2);
+    
+// }
+
+//  const result = addTwoNumbers(4,3)
+
+
+// console.log( "Result :" ,result);
+
+
+function addTwoNumbers(number1,number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+//  const result = addTwoNumbers(4,3)
+
+
+// console.log( "Result :" ,result);
+ function loginuserMessege(username = "ali"){
+    if (username===undefined){
+console.log("PLEASE ENTER A USER NAME");
+return
+    }
+
+    return `${username} just logged in`
+ }
+//  console.log(loginuserMessege("shahzaib"));
+ console.log(loginuserMessege("shahzaib"));
