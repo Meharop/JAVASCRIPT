e042384597d4ced8a552ef46ab0ece9929068adc
Meharop <141541946+Meharop@users.noreply.github.com>
2026-03-12 13:10:05 +0000

    yeahiii

--- .devcontainer/01-BASICS/functions.js
+++ .devcontainer/01-BASICS/functions.js
@@ -48,4 +48,54 @@ return
     return `${username} just logged in`
  }
 //  console.log(loginuserMessege("shahzaib"));
- console.log(loginuserMessege("shahzaib"));
+//  console.log(loginuserMessege("shahzaib"));
+
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+
+}
+// console.log(calculateCartPrice(200,300,400,5000));
+
+
+const user = {
+    username : "shahziab",
+    price: 199
+}
+function handleObject(anyobject){
+console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+// handleObject(user)
+// handleObject({
+//     username: "ali",
+//     price: 399
+// })
+
+const myNewArray = [200,344,555,666]
+function returnsecondValue(getarray){
+    return getarray[1]
+}
+console.log(returnsecondValue(myNewArray));
+
+const product1={
+
+    name: "iphone",
+    price: 50000000
+
+}
+
+const product2={
+
+    name: "samsung",
+    price: 50000000
+
+}
+function giveProduct(anyObject){
+
+    console.log(`product name ia ${anyObject.name} and tge price is ${anyObject.price}`);
+    
+}
+
+giveProduct(product1)
+giveProduct(product2)
