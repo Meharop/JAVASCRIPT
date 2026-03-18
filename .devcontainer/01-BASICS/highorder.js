468fe3e49714c36b36a9b47198d2ab60c1d17416
Meharop <141541946+Meharop@users.noreply.github.com>
2026-03-18 20:50:53 +0000

    complete

--- .devcontainer/01-BASICS/highorder.js
+++ .devcontainer/01-BASICS/highorder.js
@@ -7,12 +7,12 @@
 const arr = [1,2,3,4,5]
 
 for (const num of arr) {
-    console.log(num);
+    // console.log(num);
     
 }
 const  greeting = " I LOVE YOU BABY "
 for (const greet of greeting) {
-    console.log(`EACH CHAR ${greet}`);
+    // console.log(`EACH CHAR ${greet}`);
     
 }
 // MAP
@@ -56,33 +56,106 @@ for (const greet of greeting) {
         
     // }
 // foreach
- const coding =["js","rb","py","java","cpp"]
-
-//  coding.forEach(  function (val){console.log(val);
-//  })
-// coding.forEach(  (item ,index , arr)=> {
-
+//  const coding =["js","rb","py","java","cpp"]
+ 
+ //  coding.forEach(  function (val){console.log(val);
+ //  })
+ // coding.forEach(  (item ,index , arr)=> {
+    
 //     console.log(item) , index, arr
     
 // })
 
-const myCoding = [
+// const myCoding = [
+    
+//     {
+//         languageName : "javascript",
+//         languageFIleName : "js"
+//     },
+//     {
+    //         languageName : "java",
+    //         languageFIleName : "java"
+    //     },
+    //     {
+        //         languageName : "python",
+//         languageFIleName : "py"
+//     }
+// ]
+
+// myCoding.forEach(  (item) => {
+    //     console.log(`${item.languageFIleName} reffers to ${item.languageName}`);
+    
+    // })
+
+
+
+
+    
+    // const coding =["js","rb","py","java","cpp"]
+
+    // const values = coding.forEach(  (item) => {
+    //     // console.log(item);
+    //     return item
+    // })
+
+    // console.log(values);
+    
+
+    const myNums = [1,2,3,4,5,6,7,8,9,10]
+    //  const newNums = myNums.filter(  (num) => {
+    //    return num>5
+    //  })
+
+//     const newNum = []
+//     myNums.forEach(  (num) => {
+
+//         if(num>5){
+//             newNum.push(num)
+
+//         }
+//     })
+// console.log(newNum);
+
+/////////////////// .MAP IS AWSAM
+
+
+// const mynumbers = [1,2,3,4,5,6,7,8,9,10]
+// // const newnums= mynumbers.map(  (num) => num +10)
+// const newnum = mynumbers
+//                        .map( (num) => num * 10) 
+//                        .map( (num) => num + 1) 
+//                         .filter((num) =>num >= 15 )
+// console.log(newnum);
+
+
+///////////// REDUCE METHOD /////////////
+const Nums = [1,2,3]
+
+// const mytotal = Nums.reduce(function(acc , currval){
+//     console.log(`acc:${acc} and currval:${currval}`);
+    
+const mytotal = Nums.reduce(  ( acc , currval) => acc+currval,0)
+    
+console.log(mytotal);
+
+
+const shoppingcart = [
 
     {
-        languageName : "javascript",
-        languageFIleName : "js"
+itemname: "js cource",
+price : 999
+
     },
     {
-        languageName : "java",
-        languageFIleName : "java"
+itemname: "py cource",
+price : 3000
+
     },
     {
-        languageName : "python",
-        languageFIleName : "py"
+itemname: "c++ cource",
+price : 5999
+
     }
 ]
-
-myCoding.forEach(  (item) => {
-    console.log(`${item.languageFIleName} reffers to ${item.languageName}`);
-    
-})
+const pricepay = shoppingcart.reduce((acc,item)=>  acc + item.price,0)
+console.log(pricepay);
